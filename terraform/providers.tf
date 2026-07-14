718acc46f8bd1cdd7ab8497ed349f891276c4701
aznavneet <navneetbhardwaj935@gmail.com>
2026-07-14 17:02:48 +0000

    simplified provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,3 @@
 provider "aws" {
   region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = "AWS-AFT"
-      Environment = "POC"
-      ManagedBy   = "Terraform"
-    }
-  }
 }
