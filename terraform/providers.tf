6af97b424e9fbb2f0074a2521b61c8e68f17514f
aznavneet <navneetbhardwaj935@gmail.com>
2026-07-14 13:47:14 +0000

    updated providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,6 @@
 provider "aws" {
   region = var.aws_region
 
-  assume_role {
-    role_arn     = var.aws_role_arn
-    session_name = "gitlab-aft-session"
-  }
-
   default_tags {
     tags = {
       Project     = "AWS-AFT"
